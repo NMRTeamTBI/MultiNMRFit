79685d146580676bd02ef09357a622bd55d8b0e3
Cyril Charlier <charlier@insa-toulouse.fr>
2021-10-26 14:46:58 +0200

    :

--- NMRFit/NMRFit.py
+++ NMRFit/NMRFit.py
@@ -4,35 +4,7 @@ from Utils_nmrData import *
 from Fitting import *
 
 import sys
-################################################################
-# Test for missing librairies 
-################################################################
-# if sys.argv[1] == "test":
-#     libfn = os.path.normpath(os.path.join(sys.argv[2],"vd_hsqc_lib.txt"))
-#     txt = ""
-#     modules = ["numpy","matplotlib","nmrglue","pandas","tkinter","tqdm"]
-#     for modname in modules:
-#         try:
-#             globals()[modname] = importlib.import_module(modname)
-#             txt += "\nLibrary installed : "+str(modname) 
-#         except ImportError as e:
-#             main = Tk()
-#             main.title("Error")
-#             str_var = StringVar()
-#             def_font = tk.font.nametofont("TkDefaultFont")
-#             def_font.config(size=16)
-#             #Message Function
-#             label = Message( main, textvariable=str_var, 
-#                 relief=RAISED,width=200)
-#             str_var.set(str(modname)+" is missing") 
-#             label.pack()
-#             main.mainloop()
-#             txt += "\nLibrary missing : "+str(modname) 
-#     libf = open(libfn, 'w')
-#     libf.write(txt)
-#     libf.close()
-#     exit()
-################################################################
+
 
 if sys.argv[1:]: # Start from command line; Do not use the user interface 
     config_file = sys.argv[1]
@@ -55,158 +27,133 @@ else: # Use the user interface
     config_user = start_gui()
     Inputs_dic = config_user
 
-Analysis_Type = Inputs_dic['Data_Type']
 
 ################################################################
 # Loading Raw Data 
 ################################################################
-if Analysis_Type not in ['1D_Stack','Pseudo2D','1D']:
-    exit()
+# if Analysis_Type not in ['1D_Stack','Pseudo2D','1D']:
+#     exit()
 
-if Analysis_Type == '1D_Stack':
-    Raw_y_Data = []
-    Raw_x_Data = []    
-    Exp_List = [int(i) for i in Inputs_dic['ExpNo'].split(',')]
-    for n in Exp_List:
-        [data_1D, dic_1D, x_ppm_1D] = Read_Raw_NMR_Data_Bruker(
-                path_nmr_data   =   os.path.join(Inputs_dic['Data_Path'],Inputs_dic['Data_Folder']),
-                expno_data      =   n,
-                procno_data     =   Inputs_dic['ProcNo']
-        )       
-        
-        Raw_x_Data.append(x_ppm_1D)  
-        Raw_y_Data.append(data_1D)  
+def analysis_Pseudo2D(imputs_dic):
 
-    _dic_ = dic_1D
-    _x_ppm_ = np.array(Raw_x_Data)
-    _data_ = np.array(Raw_y_Data)
+    Analysis_Type = imputs_dic['Data_Type']
 
-elif Analysis_Type == 'Pseudo2D':
-    [data_2D, dic_2D, x_ppm_2D] = Read_Raw_NMR_Data_Bruker(
-            path_nmr_data   =   os.path.join(Inputs_dic['Data_Path'],Inputs_dic['Data_Folder']),
-            expno_data      =   Inputs_dic['ExpNo'],
-            procno_data     =   Inputs_dic['ProcNo']
+    ######################################################
+    ##################### Read and Load Data #############
+    ######################################################
+    if Analysis_Type in ['Pseudo2D','1D']:
+        [data_all, dic_all, x_ppm_all] = Read_Raw_NMR_Data_Bruker(
+                path_nmr_data   =   os.path.join(imputs_dic['Data_Path'],imputs_dic['Data_Folder']),
+                expno_data      =   imputs_dic['ExpNo'],
+                procno_data     =   imputs_dic['ProcNo']
+        )
+    elif Analysis_Type in ['1D_Stack']:
+        Raw_y_Data = []
+        Raw_x_Data = []    
+        Exp_List = [int(i) for i in imputs_dic['ExpNo'].split(',')]
+        for n in Exp_List:
+            [data, diC, x_ppm] = Read_Raw_NMR_Data_Bruker(
+                    path_nmr_data   =   os.path.join(imputs_dic['Data_Path'],imputs_dic['Data_Folder']),
+                    expno_data      =   n,
+                    procno_data     =   imputs_dic['ProcNo']
+            )       
+            
+            Raw_x_Data.append(x_ppm)  
+            Raw_y_Data.append(data)  
+
+        x_ppm_all = np.array(Raw_x_Data)
+        data_all = np.array(Raw_y_Data)
+    #-----------------------------------------------------#    
+
+    ######################################################
+    #Extract data within the region selected by the user##
+    ######################################################
+    [_Ext_data_, _Ext_x_ppm_] = Extract_Data(
+        data     = data_all,
+        x_ppm    = x_ppm_all,
+        x_lim    = [imputs_dic['Spec_Lim'        ][0],imputs_dic['Spec_Lim'   ][1]]
     )
+    #-----------------------------------------------------#   
 
-    _dic_ = dic_2D
-    _x_ppm_ = x_ppm_2D
-    _data_ = data_2D
+    ######################################################
+    ###########Extract the reference spectrum#############
+    ######################################################
+    if Analysis_Type == 'Pseudo2D':
+        y_Spec_init_ = _Ext_data_[int(imputs_dic['Ref_Spec' ])-1,:]
+        x_Spec_init_ = _Ext_x_ppm_
+    elif Analysis_Type == '1D_Stack':
+        y_Spec_init_ = _Ext_data_[int(imputs_dic['Ref_Spec' ])-1,:]
+        x_Spec_init_ = _Ext_x_ppm_[int(imputs_dic['Ref_Spec' ])-1,:]
+    elif Analysis_Type == '1D':
+        y_Spec_init_ = _Ext_data_
+        x_Spec_init_ = _Ext_x_ppm_
+    #-----------------------------------------------------#   
 
-elif Analysis_Type == '1D':
-    [data_1D, dic_1D, x_ppm_1D] = Read_Raw_NMR_Data_Bruker(
-            path_nmr_data   =   os.path.join(Inputs_dic['Data_Path'],Inputs_dic['Data_Folder']),
-            expno_data      =   Inputs_dic['ExpNo'],
-            procno_data     =   Inputs_dic['ProcNo']
-    )
-    _dic_ = dic_1D
-    _x_ppm_ = x_ppm_1D
-    _data_ = data_1D
-
-################################################################
-# Extract 2D Data in the region of interest 
-################################################################
-[_Ext_data_, _Ext_x_ppm_] = Extract_Data(
-    data     = _data_,
-    x_ppm    = _x_ppm_,
-    x_lim    = [Inputs_dic['Spec_Lim'        ][0],Inputs_dic['Spec_Lim'   ][1]]
-)
-
-# ################################################################
-# # Extract 1D Data for intitial fitting 
-# ################################################################
-if Inputs_dic['Data_Type'] == 'Pseudo2D':
-    y_Spec_init_ = _Ext_data_[int(Inputs_dic['Ref_Spec' ])-1,:]
-    x_Spec_init_ = _Ext_x_ppm_
-if Inputs_dic['Data_Type'] == '1D_Stack':
-    y_Spec_init_ = _Ext_data_[int(Inputs_dic['Ref_Spec' ])-1,:]
-    x_Spec_init_ = _Ext_x_ppm_[int(Inputs_dic['Ref_Spec' ])-1,:]
-if Inputs_dic['Data_Type'] == '1D':
-    y_Spec_init_ = _Ext_data_
-    x_Spec_init_ = _Ext_x_ppm_
-
-################################################################
-# Initial 1D Peak Picking 
-################################################################
-Peak_Picking = Peak_Picking_1D(
+    ######################################################
+    ####################Peak Picking######################
+    ######################################################
+    Peak_Picking = Peak_Picking_1D(
     x_data          =   x_Spec_init_, 
     y_data          =   y_Spec_init_, 
-    threshold       =   Inputs_dic['Threshold'],
-)
-# ################################################################
-# # Manual Check of Peak Picking
-# ################################################################
-new_th, pp_res_Sel = main_window(x_Spec_init_,y_Spec_init_,Inputs_dic['Threshold'],Peak_Picking)
-# ################################################################
-# # Manual Check of Peak Picking 7 new Peak picking if user asks for it
-# ################################################################
-check_for_nt = new_th.isnull().values.any()
-while check_for_nt == False:
-    new_th = new_th.apply(pd.to_numeric, errors='coerce')
-    pp_t = new_th.nt.values[0]
-
-    Peak_Picking = Peak_Picking_1D(
-        x_data          =   x_Spec_init_, 
-        y_data          =   y_Spec_init_, 
-        threshold       =   pp_t,
+    threshold       =   imputs_dic['Threshold'],
     )
-    
-    new_th, pp_res_Sel = main_window(x_Spec_init_,y_Spec_init_,pp_t,Peak_Picking)
+    #-----------------------------------------------------#   
+
+    ######################################################
+    #############Manual Check of Peak Picking#############
+    ######################################################
+    new_th, pp_res_Sel = main_window(x_Spec_init_,y_Spec_init_,imputs_dic['Threshold'],Peak_Picking)
     check_for_nt = new_th.isnull().values.any()
-    if check_for_nt == True:
-        break  
-cluster_list =  pp_res_Sel.Cluster.unique()
+    while check_for_nt == False:
+        new_th = new_th.apply(pd.to_numeric, errors='coerce')
+        pp_t = new_th.nt.values[0]
 
-################################################################
-# Initial 1D Peak Fitting 
-################################################################
-
-if Inputs_dic['Data_Type'] == '1D':
-    _1D_Fit_ = Fitting_Function(
-            x_Spec_init_,
-            pp_res_Sel,
-            y_Spec_init_)
-    d_id = Initial_Values(pp_res_Sel, x_Spec_init_)[0]
-    sim = simulate_data(
-            x_Spec_init_,
-            pp_res_Sel,
-            _1D_Fit_.x.tolist(),
-            d_id)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(x_Spec_init_,y_Spec_init_,'o')
-    ax.plot(x_Spec_init_,sim, 'r-', label='fit')
-
-    ax.invert_xaxis()
-    ax.set_xlabel(r'$^1H$ $(ppm)$')
-    plt.show()
-    plt.close()
-    exit()
-
-if Inputs_dic['Data_Type'] in ['1D_Stack','Pseudo2D']:
-    Fit_results = Pseudo2D_PeakFitting(
-                Intensities  =   _Ext_data_,
-                x_Spec       =   x_Spec_init_,
-                ref_spec     =   Inputs_dic['Ref_Spec'],
-                peak_picking_data = pp_res_Sel
-               # scaling = Inputs_dic['Scaling']
+        Peak_Picking = Peak_Picking_1D(
+            x_data          =   x_Spec_init_, 
+            y_data          =   y_Spec_init_, 
+            threshold       =   pp_t,
         )
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-#     print(Fit_results)
-################################################################
-# Plot of the fit 
-################################################################
-Plot_All_Spectrum(
-    pdf_path = Inputs_dic['pdf_path'],
-    pdf_folder = Inputs_dic['pdf_folder'],
-    pdf_name = Inputs_dic['pdf_name'],
-    Fit_results= Fit_results,
-    Int_Pseudo_2D_Data = _Ext_data_,
-    x_ppm = x_Spec_init_,
-    Peak_Picking_data= pp_res_Sel
-    #scaling = Inputs_dic['Scaling']
-    #Peak_Type= d_mapping[Peak_Type_0_]["f_function"]
-)
+        
+        new_th, pp_res_Sel = main_window(x_Spec_init_,y_Spec_init_,pp_t,Peak_Picking)
+        check_for_nt = new_th.isnull().values.any()
+        if check_for_nt == True:
+            break  
+    cluster_list =  pp_res_Sel.Cluster.unique()
+    #-----------------------------------------------------#   
 
-print('Full Analysis is done')
-print('#--------#')  
-print('##########')
+    ######################################################
+    #######################Fitting########################
+    ######################################################
+    if Analysis_Type in ['1D','1D_Stack','Pseudo2D']:
+        Fit_results = Pseudo2D_PeakFitting(
+                    Intensities  =   _Ext_data_,
+                    x_Spec       =   x_Spec_init_,
+                    ref_spec     =   imputs_dic['Ref_Spec'],
+                    peak_picking_data = pp_res_Sel
+            )
+
+    #-----------------------------------------------------#   
+    ######################################################
+    #######################Output#########################
+    ######################################################
+    Plot_All_Spectrum(
+        pdf_path = imputs_dic['pdf_path'],
+        pdf_folder = imputs_dic['pdf_folder'],
+        pdf_name = imputs_dic['pdf_name'],
+        Fit_results= Fit_results,
+        Int_Pseudo_2D_Data = _Ext_data_,
+        x_ppm = x_Spec_init_,
+        Peak_Picking_data= pp_res_Sel
+    )
+    print('Full Analysis is done')
+    print('#--------#')  
+    print('##########')
+    return Peak_Picking
+
+
+analysis_Pseudo2D(Inputs_dic)
+exit()
+
+
+
+
