214cd3e6e36dfd21fb4d9ccbc657f21f8943579b
Cyril Charlier <charlier@insa-toulouse.fr>
2021-12-14 14:05:06 +0100

    remove build and dist folder

--- build/lib/nmrfit/__main__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import nmrfit.ui
-
-# start CLI if arguments provided, otherwise switch to GUI
-if len(sys.argv) > 1:
-    nmrfit.ui.start_cli(sys.argv)
-else:
-    nmrfit.ui.start_gui()
