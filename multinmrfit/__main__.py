2ac7cd1a06017ea6bdd52b1090a366569cba79cb
pierremillard <millard@insa-toulouse.fr>
2022-03-07 10:44:29 +0100

    update main

--- multinmrfit/__main__.py
+++ multinmrfit/__main__.py
@@ -3,9 +3,11 @@ import multinmrfit.ui as nui
 import multinmrfit.io as nio
 import multinmrfit.run as nrun
 
+
 if not len(sys.argv) == 2:
     app = nui.App(user_input=nio.create_user_input())
     app.start()
 else:
-    user_input = nio.load_config_file(None,config_file_path=sys.argv[1])        
+    user_input = nio.load_config_file(None,config_file_path=sys.argv[1]) 
+    user_input = nio.check_input_file(user_input,None)       
     nrun.run_analysis(user_input,None)
