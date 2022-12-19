e81c7ad1ecee734b28133377813c31f3af3d7f73
pierremillard <millard@insa-toulouse.fr>
2022-12-19 12:10:21 +0100

    cleaning/organizing

--- multinmrfit/logger.py
+++ multinmrfit/logger.py
@@ -1,15 +1,15 @@
-"""Logger module containing the PhysioFit logger setup"""
+"""Logger module containing the MultiNMRFit logger setup"""
 
 import logging
 
 # Setup base logger
 
-mod_logger = logging.getLogger("PhysioFit_Logger")
+mod_logger = logging.getLogger("MultiNMRFit_Logger")
 mod_logger.setLevel(logging.DEBUG)
 
 
 def initialize_fitter_logger(verbose):
-    logger = logging.getLogger(f"PhysioFit_Logger.base.fitter.PhysioFitter")
+    logger = logging.getLogger(f"MultiNMRFit_Logger.base.fitter.SpectrumFitter")
     logger.setLevel(logging.DEBUG)
 
     return logger
