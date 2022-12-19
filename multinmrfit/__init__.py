5a300fad2c577982d384fda2b8f17c4e27f94b6f
pierremillard <millard@insa-toulouse.fr>
2022-12-19 11:54:19 +0100

    clean repo

--- multinmrfit/__init__.py
+++ multinmrfit/__init__.py
@@ -3,5 +3,5 @@ NMRFit package initialisation.
 """
 
 # Version number MUST be maintained here (x.y.z format)
-__version__ = '0.1.1'
+__version__ = '2.0.0a'
 
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     url="https://github.com/NMRTeamTBI/NMRFit",
     packages=setuptools.find_packages(),
     python_requires='>=3.5',
-    install_requires=['pandas>=0.17.1', 'scipy>=0.12.1', 'matplotlib>=3.3.4','nmrglue>=0.6', 'numpy>=1.14.0', 'PyPDF2>=1.26.0', 'natsort>=8.1.0'],
+    install_requires=['pandas>=0.17.1', 'scipy>=0.12.1', 'nmrglue>=0.6', 'numpy>=1.14.0'],
     package_data={'': ['data/*.png', ], },
     include_package_data=True,
     classifiers=[
@@ -28,17 +28,18 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        ],
-    entry_points={
-        'console_scripts': [
-            'multinmrfit = multinmrfit.run:main'
-        ],
-        'gui_scripts': [
-            'multinmrfit = multinmrfit.run:main'
-        ]
-    }
+        ]#,
+    #entry_points={
+    #    'console_scripts': [
+    #        'multinmrfit = multinmrfit.run:main'
+    #    ],
+    #    'gui_scripts': [
+    #        'multinmrfit = multinmrfit.run:main'
+    #    ]
+    #}
 )
