c7020a28ea76a0f191e2821f7eab56cff727a81e
Cyril Charlier <charlier@insa-toulouse.fr>
2024-02-21 23:56:44 +0100

    Fic txt data import

--- multinmrfit/base/process.py
+++ multinmrfit/base/process.py
@@ -185,8 +185,8 @@ class Process(object):
         if "ppm" not in self.txt_data.columns:
             raise ValueError("Column 'ppm' missing")
         ppm = self.txt_data.ppm.values.tolist()
-        data = np.array(self.txt_data.loc[:, self.txt_data.columns != 'ppm'])
-
+        data = np.array(self.txt_data.loc[:, self.txt_data.columns != 'ppm']).T
+        
         return ppm, data
 
     def load_2D_spectrum(self):
