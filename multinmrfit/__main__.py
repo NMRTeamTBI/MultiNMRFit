f9a34d94ddb93138e55e2751cd377479b105d4d8
pierremillard <millard@insa-toulouse.fr>
2024-02-29 22:29:19 +0100

    update entry point

--- multinmrfit/__main__.py
+++ multinmrfit/__main__.py
@@ -6,3 +6,6 @@ def main():
     path_to_app = Path(multinmrfit.__file__).parent
     path_to_app = path_to_app / "ui" / "01_Inputs_&_Outputs.py"
     run(["streamlit", "run", str(path_to_app)])
+
+if __name__ == '__main__':
+    main()
