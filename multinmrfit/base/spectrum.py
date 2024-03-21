fix: scaling factor & first spectrum in pseudo2D

multinmrfit/base/spectrum.py
@@ -458,11 +458,12 @@ class Spectrum(object):
         self._check_parameters()
         
         # set scaling factor to stabilize convergence
-        scaling_factor = np.mean(self.intensity)
+        mean_sp = np.mean(self.intensity)
+        scaling_factor = mean_sp if mean_sp > 1 else 1
 
         # apply scaling factor on parameters (intensity & offset)
         params_scaled = self.params.copy(deep=True)
-        params_scaled.loc[params_scaled['par'].isin(["intensity", "offset"]), ["ini", "lb", "ub"]] /= abs(scaling_factor)
+        params_scaled.loc[params_scaled['par'].isin(["intensity", "offset"]), ["ini", "lb", "ub"]] /= scaling_factor
         
         # set initial values
         x0 = params_scaled['ini'].values.tolist()
@@ -471,7 +472,7 @@ class Spectrum(object):
         bounds = list(zip(params_scaled['lb'], params_scaled['ub']))
 
         # scale data
-        data_scaled = self.intensity / abs(scaling_factor)
+        data_scaled = self.intensity / scaling_factor
 
         # fit spectrum
         if method == "differential_evolution":
@@ -509,7 +510,7 @@ class Spectrum(object):
         self.params['opt_sd'] = standard_deviations
 
         # scale back estimated parameters
-        self.params.loc[self.params['par'].isin(["intensity", "offset"]), ["opt", "opt_sd"]] *= abs(scaling_factor)
+        self.params.loc[self.params['par'].isin(["intensity", "offset"]), ["opt", "opt_sd"]] *= scaling_factor
 
         # simulate spectrum from estimated parameters
         self.fit_results.best_fit = self.simulate(self.params['opt'].values.tolist())