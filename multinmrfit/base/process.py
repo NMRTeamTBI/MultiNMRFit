a39f59b3729526fa92678f7d16a042a75e821a9d
pierremillard <millard@insa-toulouse.fr>
2024-08-27 14:42:55 +0200

    update lb on J

--- multinmrfit/base/process.py
+++ multinmrfit/base/process.py
@@ -392,6 +392,8 @@ class Process(object):
         params.loc[mask, "lb"] = 0.0
         mask = params['par'].isin(["gl"])
         params.loc[mask, "lb"] = 0.0
+        mask = params['par'].isin(["J", "J1", "J2", "J3"])
+        params.loc[mask, "lb"] = 0.0
         mask = params['par'].isin(['intensity'])
         params.loc[mask, "lb"] = 1.0
 
