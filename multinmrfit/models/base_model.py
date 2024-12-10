9d83b41a3c00dad343ac1434095f3c094cf7875e
pierremillard <millard@insa-toulouse.fr>
2024-12-10 22:27:27 +0100

    update docstrings of base model

--- multinmrfit/models/base_model.py
+++ multinmrfit/models/base_model.py
@@ -1,13 +1,26 @@
-from __future__ import annotations
+from __future__ import annotations  # Importing annotations from future versions of Python
 
-import numpy as np
-import pandas as pd
+import numpy as np  # Importing the numpy library for numerical operations
+import pandas as pd  # Importing the pandas library for data manipulation
 
 
 class Model(object):
+    """
+    A base class for models.
+
+    Attributes:
+        name (str): The name of the model.
+        description (str): The description of the model.
+        _params (pandas.DataFrame): Private variable to store the model parameters.
+        _cnstr_wd (pandas.DataFrame): Private variable to store the constraint window.
+        _par_idx (None): Private variable to store the parameter index.
+        peak_number (None): The number of peaks in the model.
+    """
 
     def __init__(self):
-
+        """
+        Initializes the Model object.
+        """
         self.name = None
         self.description = None
         self._params = None
@@ -16,14 +29,36 @@ class Model(object):
         self.peak_number = None
 
     def set_default_params(self):
+        """
+        Sets the default parameters for the model.
+
+        Returns:
+            pandas.DataFrame: The default parameters for the model.
+        """
         self._params = pd.DataFrame(dict((k, self.default_params[k]) for k in ('model', 'par', 'ini', 'lb', 'ub')))
         return self._params
 
     def set_default_cnstr_wd(self):
+        """
+        Sets the default constraint window for the model.
+
+        Returns:
+            pandas.DataFrame: The default constraint window for the model.
+        """
         self._cnstr_wd = pd.DataFrame(dict((k, self.default_params[k]) for k in ('model', 'par', 'shift_allowed', 'relative')))
         return self._cnstr_wd
 
     def set_params(self, name: str, val: tuple):
+        """
+        Sets the value of a parameter for the model.
+
+        Args:
+            name (str): The name of the parameter.
+            val (tuple): The (key,value) pair to set for the parameter.
+
+        Raises:
+            ValueError: If the parameter or key is not found.
+        """
         try:
             self._params.loc[self._params["par"] == name, val[0]] = val[1]
         except:
@@ -33,6 +68,16 @@ class Model(object):
                 raise ValueError("key '{}' not found".format(val[0]))
 
     def integrate(self, params: list, ppm: list):
+        """
+        Calculates the integral of the simulated spectra.
+
+        Args:
+            params (list): The parameters for the model.
+            ppm (list): The ppm values.
+
+        Returns:
+            float: The calculated integral.
+        """
         sim_spectra = self.simulate(params, ppm)
         integral = np.trapz(y=sim_spectra, x=ppm)
         return integral
